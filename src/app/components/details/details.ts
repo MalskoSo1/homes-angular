c501013fd05cc817b0e307af2883b92701f765e9
liza <hotskoelizabeth@gmail.com>
2026-05-20 15:00:06 +0300

    feat: add house details and routes

--- src/app/components/details/details.ts
+++ src/app/components/details/details.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HousingLocationInfo } from "src/app/interfaces/housinglocation";
+import { Housing } from "src/app/services/housing";
 @Component({
-  selector: 'app-details',
-  imports: [],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
-  styles: ``,
+  selector: "app-details",
+  templateUrl: "./details.html",
+  styleUrls: ["./details.css"],
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(Housing);
+  housingLocation: HousingLocationInfo | undefined;
 
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
